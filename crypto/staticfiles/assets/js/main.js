22e9f7b3608b4e93edd8751b9b1c79e74fc6a253
Asralkhanov Zikrilloh <84511686+asralkhanov2007@users.noreply.github.com>
2022-04-23 10:37:09 +0500

    No cometnt
    no desc

--- crypto/staticfiles/assets/js/main.js
+++ crypto/staticfiles/assets/js/main.js
@@ -1,30 +1,27 @@
 // Объект с курсами криптоВалют
-const rates = {}
+const rates = {
+    "BITCOIN":180,
+    "NEO":120,
+    "DASH":100,
 
-// Элементы для отображения курса криптоВалют
-const elementUSD = 100 ;
-const elementEUR = 150 ;
-const elementGBP = 200 ;
+}
 
 // Элементы формы, ввод суммы, выбор валюты, поле с резльтатом
 const input = document.querySelector('#input');
 const result = document.querySelector('#result');
 const select = document.querySelector('#select');
 
-getCurrecies()
 
-async function getCurrecies() {
-
-    rates.USD = 100
-    rates.EUR = 200
-    rates.GBP = 50
+input.oninput = function(){
+    console.log('Changed');
+    result.value = (parseFloat(input.value) / rates[select.value].Value).toFixed(2);
     
-    console.log(rates);
 }
 
-input.oninput = function() {
-    console.log('Changed!');
-    result.value = (parseFloat(nput.value) / rates[select.value].Value).toFixed(2);
+select.oninput = function(){
+    console.log('Changed');
+    result.value = (parseFloat(input.value) / rates[select.value].Value).toFixed(2);
+
 }
 
 // input.oninput = convertValue;
