1cac13b1a84df83b69b246f6cd9e30d779c6970a
Asralkhanov Zikrilloh <84511686+asralkhanov2007@users.noreply.github.com>
2022-04-23 11:34:27 +0500

    Finaly I've finished
    This is the last commit So today I'll send my work

--- crypto/staticfiles/assets/js/main.js
+++ crypto/staticfiles/assets/js/main.js
@@ -1,37 +1,46 @@
-// Объект с курсами криптоВалют
-const rates = {
-    "BITCOIN":180,
-    "NEO":120,
-    "DASH":100,
 
+window.onload = function () {
+    let c = {'BITCOIN':'200', 'DASH':'150', 'NEO':'4'}; // Устанавливаем курс валют
+
+    let val = document.getElementById('input'); // Получаем элемент ввода данных
+    let select = document.getElementById('select'); // Получаем первый селект
+    let select2 = document.getElementById('select2'); // Получаем второй селект
+    let result = document.getElementsByClassName('result')[0]; // Получаем поле куда будем писать результат
+    function summ() { // Делаем функцию
+        let z = 0;
+        if(select.value === select2.value){ // Если оба значения в селектах равны
+            result.innerText = val.value; // То просто вписываем данные из поля ввода
+        } else {
+            if(select.value != 'BITCOIN'){ // Если не равны рублю, то
+                z = val.value*c[select.value]; // Переводим сумму в рубли
+                result.innerHTML = Math.ceil((z/c[select2.value])*100)/100; // Делим на курс и округляем до сотых
+            } else { // Если не равны
+                result.innerHTML = Math.ceil((val.value*c[select2.value])*100)/100; // Умножаем на курс и округляем до сотых
+            }
+        }
+    }
+    val.oninput = function () { // При вводе данных в поле вызываем функцию.
+        summ();
+    };
+    select.onchange = function () { // При смене первого селекта вызываем функцию.
+        summ();
+    };
+    select2.onchange = function () { // При смене второго селекта вызываем функцию.
+        summ();
+    }
 }
 
-// Элементы формы, ввод суммы, выбор валюты, поле с резльтатом
-const input = document.querySelector('#input');
-const result = document.querySelector('#result');
-const select = document.querySelector('#select');
-
-
-input.oninput = function(){
-    console.log('Changed');
-    result.value = (parseFloat(input.value) / rates[select.value].Value).toFixed(2);
-    
-}
-
-select.oninput = function(){
-    console.log('Changed');
-    result.value = (parseFloat(input.value) / rates[select.value].Value).toFixed(2);
-
-}
-
-// input.oninput = convertValue;
-
-// select.oninput = convertValue;
-
-// // Функция конвертации
-// function convertValue() {
-
-//     result.value = (parseFloat(input.value) / rates[select.value].Value).toFixed(2);
-
-// }
-
+function myFunction() {
+    /* Получить текстовое поле */
+    var copyText = document.getElementById("myInput");
+  
+    /* Выделите текстовое поле */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* Для мобильных устройств */
+  
+    /* Скопируйте текст внутри текстового поля */
+    document.execCommand("copy");
+  
+    /* Оповещение скопированного текста */
+    alert("Скопировал текст: " + copyText.value);
+  }
